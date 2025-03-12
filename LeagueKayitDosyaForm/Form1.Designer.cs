deb175c709b9e89ccf6883dbe21a0c57d9aa1a36
Elanur Yurter <33anurr.yyurter@gmail.com>
2025-03-12 14:30:40 +0300

    Add project files.

---
+++ LeagueKayitDosyaForm/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace LeagueKayitDosyaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_karakterler = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_aciklama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cb_rol = new System.Windows.Forms.ComboBox();
+            this.cb_koridor = new System.Windows.Forms.ComboBox();
+            this.tb_isim = new System.Windows.Forms.TextBox();
+            this.btn_kaydet = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_karakterler
+            // 
+            this.lb_karakterler.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lb_karakterler.FormattingEnabled = true;
+            this.lb_karakterler.ItemHeight = 24;
+            this.lb_karakterler.Location = new System.Drawing.Point(627, 260);
+            this.lb_karakterler.Name = "lb_karakterler";
+            this.lb_karakterler.Size = new System.Drawing.Size(342, 340);
+            this.lb_karakterler.TabIndex = 23;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LeagueKayitDosyaForm.Properties.Resources.League_of_Legends_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(321, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 201);
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_aciklama
+            // 
+            this.tb_aciklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_aciklama.Location = new System.Drawing.Point(244, 398);
+            this.tb_aciklama.Multiline = true;
+            this.tb_aciklama.Name = "tb_aciklama";
+            this.tb_aciklama.Size = new System.Drawing.Size(278, 96);
+            this.tb_aciklama.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(123, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 24);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Açıklaması =";
+            // 
+            // cb_rol
+            // 
+            this.cb_rol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cb_rol.FormattingEnabled = true;
+            this.cb_rol.Location = new System.Drawing.Point(244, 359);
+            this.cb_rol.Name = "cb_rol";
+            this.cb_rol.Size = new System.Drawing.Size(278, 32);
+            this.cb_rol.TabIndex = 19;
+            // 
+            // cb_koridor
+            // 
+            this.cb_koridor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cb_koridor.FormattingEnabled = true;
+            this.cb_koridor.Location = new System.Drawing.Point(244, 316);
+            this.cb_koridor.Name = "cb_koridor";
+            this.cb_koridor.Size = new System.Drawing.Size(278, 32);
+            this.cb_koridor.TabIndex = 18;
+            // 
+            // tb_isim
+            // 
+            this.tb_isim.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_isim.Location = new System.Drawing.Point(244, 280);
+            this.tb_isim.Name = "tb_isim";
+            this.tb_isim.Size = new System.Drawing.Size(278, 29);
+            this.tb_isim.TabIndex = 17;
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kaydet.Location = new System.Drawing.Point(418, 525);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(104, 36);
+            this.btn_kaydet.TabIndex = 16;
+            this.btn_kaydet.Text = "Kaydet";
+            this.btn_kaydet.UseVisualStyleBackColor = true;
+            this.btn_kaydet.Click += new System.EventHandler(this.btn_kaydet_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(115, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 24);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Karaker Adı =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(151, 324);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Koridor =";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(184, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Rol =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 647);
+            this.Controls.Add(this.lb_karakterler);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tb_aciklama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cb_rol);
+            this.Controls.Add(this.cb_koridor);
+            this.Controls.Add(this.tb_isim);
+            this.Controls.Add(this.btn_kaydet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_karakterler;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tb_aciklama;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cb_rol;
+        private System.Windows.Forms.ComboBox cb_koridor;
+        private System.Windows.Forms.TextBox tb_isim;
+        private System.Windows.Forms.Button btn_kaydet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LeagueKayitDosyaForm/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeagueKayitDosyaForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cb_koridor.Items.Add("Üst");
+            cb_koridor.Items.Add("Orta");
+            cb_koridor.Items.Add("Alt");
+            cb_koridor.Items.Add("Orman");
+
+            cb_rol.Items.Add("Nişancı");
+            cb_rol.Items.Add("Orman");
+            cb_rol.Items.Add("Destek");
+            cb_rol.Items.Add("Büyücü");
+            cb_rol.Items.Add("Tank");
+            cb_rol.Items.Add("Suikastçı");
+        }
+
+        private void btn_kaydet_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tb_isim.Text))
+            {
+                string isim = tb_isim.Text;
+                string kayityolu = "Karakterler/" + isim + ".txt";
+
+                StreamWriter yazici = new StreamWriter(kayityolu);
+                yazici.WriteLine(tb_isim.Text);
+                yazici.WriteLine(cb_koridor.Text);
+                yazici.WriteLine(cb_rol.Text);
+                yazici.WriteLine(tb_aciklama.Text);
+            }
+            else
+            {
+                MessageBox.Show("Karakter adı boş bırakılamaz!", "hata!");
+            }
+        }
+        public void DosyalarıGetir()
+        {
+            DirectoryInfo di = new DirectoryInfo("Karakterler/");
+            lb_karakterler.Items.Clear();
+            foreach (FileInfo item in  di.GetFiles())
+            {
+                string dosyaadi = item.Name;
+                lb_karakterler.Items.Add(dosyaadi.Substring(0, dosyaadi.Length - 4));
+            }
+        }
+    }
+}
